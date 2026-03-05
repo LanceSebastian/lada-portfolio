535119b8040a80a4bc5de11c46a61557d5a10150
Lance Vincent Sebastian <lvsebastian2002@gmail.com>
2026-03-05 19:49:33 +0000

    feat: add Socials to navbar, center title

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,15 +7,16 @@ const links = [
   { to: '/contact', label: 'Contact' },
 ]
 
+const social = [
+  { to: 'https://www.instagram.com/sritongbaiarts/', icon: 'fa-brands fa-instagram'},
+]
+
 export default function Nav() {
   const { pathname } = useLocation()
 
   return (
     <header className="sticky top-0 z-50 bg-bg border-b border-stone-200">
-      <nav className="max-w-5xl mx-auto px-6 py-8 flex items-center justify-between">
-        <Link to="/" className="font-display text-text font-semibold tracking-wide text-2xl">
-          Lada Sritongbai
-        </Link>
+      <nav className=" max-w-screen mx-auto px-6 py-8 grid grid-cols-3 items-center justify-between">
         <ul className="flex gap-8">
           {links.map(({ to, label }) => (
             <li key={to}>
@@ -28,6 +29,25 @@ export default function Nav() {
                 }`}
               >
                 {label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <Link to="/" className="font-display text-text font-semibold tracking-wide text-4xl text-center">
+          Lada Sritongbai
+        </Link>
+        <ul className="flex gap-8 justify-end">
+          {social.map(({ to, icon }) => (
+            <li key={to}>
+              <Link
+                to={to}
+                className={`text-3xl tracking-wide transition-colors duration-200 ${
+                  pathname === to
+                    ? 'text-stone-900 font-medium'
+                    : 'text-stone-400 hover:text-stone-900'
+                }`}
+              >
+                <i className={icon}></i>
               </Link>
             </li>
           ))}
