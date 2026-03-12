bc833d2a5af20cb4133a63677f60ddd21b4061f8
Lance Vincent Sebastian <lvsebastian2002@gmail.com>
2026-03-12 21:08:38 +0000

    feat: add pre loaded dots for desktop and mobile

---
+++ src/data/dots-mobile.js
@@ -0,0 +1,16 @@
+const dotsMobile = [
+  { id: 0,  x: 0.16,  y: 10.76,  delay: 9.31,  size: 72.06 },
+  { id: 1,  x: 53.13, y: 3.81,  delay: 7.56,  size: 54.62 },
+  { id: 2,  x: 69.86, y: 19.96,  delay: 5.05,  size: 69.11 },
+  { id: 3,  x: 8.72,  y: 27.44, delay: 5.57,  size: 38.32 },
+  { id: 4,  x: 35.49, y: 20.24, delay: 8.57,  size: 48.5  },
+  { id: 5,  x: 75.06, y: 63.05, delay: 3.38,  size: 25.75 },
+  { id: 6,  x: 3.29,  y: 61.33, delay: 2.92,  size: 52.52 },
+  { id: 7,  x: 41.49, y: 54.06, delay: 0.06,  size: 34.23 },
+  { id: 8,  x: 72.04, y: 56.12, delay: 9.76,  size: 38.43 },
+  { id: 9,  x: 5.56,  y: 75.64, delay: 3.15,  size: 22.48 },
+  { id: 10, x: 36.16, y: 76.41, delay: 9.09,  size: 55.16 },
+  { id: 11, x: 76.27, y: 78.05, delay: 10.64, size: 55.22 },
+]
+
+export default dotsMobile
