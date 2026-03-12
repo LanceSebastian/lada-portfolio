b30bb7e53953aa1c8aa9bb06b5f158f85a8043c3
Lance Vincent Sebastian <lvsebastian2002@gmail.com>
2026-03-12 19:18:15 +0000

    feat: add images to gallery

--- src/data/gallery.js
+++ src/data/gallery.js
@@ -2,5 +2,8 @@ const gallery = [
 { id: 1, title:"", description: "", link:"/gallery/IMG_1478.PNG"},
 { id: 2, title:"", description: "", link:"/gallery/IMG_1479.PNG"},
 { id: 3, title:"", description: "", link:"/gallery/IMG_1480.PNG"},
+{ id: 4, title:"", description: "", link:"/gallery/IMG_1487.PNG"},
+{ id: 5, title:"", description: "", link:"/gallery/IMG_1488.PNG"},
+{ id: 6, title:"", description: "", link:"/gallery/IMG_1489.PNG"}
 ]
 export default gallery
