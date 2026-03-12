9962e50061bcedb4c88286fc8208c40c62b9d34c
Lance Vincent Sebastian <lvsebastian2002@gmail.com>
2026-03-12 21:16:32 +0000

    fix: update the dots to fit new height size

--- src/data/dots-mobile.js
+++ src/data/dots-mobile.js
@@ -4,13 +4,14 @@ const dotsMobile = [
   { id: 2,  x: 69.86, y: 19.96,  delay: 5.05,  size: 69.11 },
   { id: 3,  x: 8.72,  y: 27.44, delay: 5.57,  size: 38.32 },
   { id: 4,  x: 35.49, y: 20.24, delay: 8.57,  size: 48.5  },
-  { id: 5,  x: 75.06, y: 63.05, delay: 3.38,  size: 25.75 },
+  { id: 5,  x: 75.06, y: 78.05, delay: 3.38,  size: 25.75 },
   { id: 6,  x: 3.29,  y: 61.33, delay: 2.92,  size: 52.52 },
-  { id: 7,  x: 41.49, y: 54.06, delay: 0.06,  size: 34.23 },
+  { id: 7,  x: 41.49, y: 64.06, delay: 0.06,  size: 34.23 },
   { id: 8,  x: 72.04, y: 56.12, delay: 9.76,  size: 38.43 },
-  { id: 9,  x: 5.56,  y: 75.64, delay: 3.15,  size: 22.48 },
-  { id: 10, x: 36.16, y: 76.41, delay: 9.09,  size: 55.16 },
-  { id: 11, x: 76.27, y: 78.05, delay: 10.64, size: 55.22 },
+  { id: 9,  x: 5.56,  y: 85.64, delay: 3.15,  size: 22.48 },
+  { id: 10, x: 36.16, y: 80.41, delay: 9.09,  size: 55.16 },
+  { id: 11, x: 76.27, y: 88.05, delay: 10.64, size: 55.22 },
+  { id: 12, x: 28.27, y: 8.05, delay: 10.64, size: 22.48 },
 ]
 
 export default dotsMobile
